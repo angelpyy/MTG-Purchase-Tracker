83db040563b13c21f6f1cce763905f1d2df38017
angel gutierrez <56939943+angelpyy@users.noreply.github.com>
2023-06-01 10:30:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
+# program does not handle card variants (i did not save foil data or extended)
+# but we can do secret lairs :)
+import util
+import pandas as pd
 import tkinter as tk
 import csv
 
-def create_csv_window():
-    # Create a new Tkinter window
-    window = tk.Tk()
-    window.geometry("1200x800")
-    window.title("CSV Viewer")
+# TODO: csv editor? for some reason? i'm not sure its really necessary for what i want
+# tkinter gui :( // this sounds really hard and unnecessary :(
 
-    # Open the file
-    with open('order history/mtg_stocks.csv', mode='r') as file:
-        # Read the CSV file
-        csvFile = csv.reader(file)
+# Open Tkinter window
+window = tk.Tk()
+window.geometry("1200x800")
+window.title("Stop Spending on Cardboard")
 
-        # Iterate over the lines of the CSV file
-        for i, line in enumerate(csvFile):
-            # Iterate over the values in each line
-            for j, val in enumerate(line):
-                # Create a label widget for each val
-                label = tk.Label(window, text=val)
-                label.grid(row=i, column=j)
+# open the file :p
+with open('order history/mtg_stocks.csv', mode ='r')as file:
+    # reading the CSV file
+    csvFile = csv.reader(file)
 
-    # Run the Tkinter event loop
-    window.mainloop()
+    # Iterate over the lines of the CSV file
+    for i, line in enumerate(csvFile):
+        # Iterate over the values in each line
+        for j, val in enumerate(line):
+            # Create a label widget for each val
+            label = tk.Label(window, text=val)
+            label.grid(row=i, column=j)
 
-# Call the function to create the window
-create_csv_window()
+window.mainloop()
