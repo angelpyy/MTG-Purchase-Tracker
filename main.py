d44cdcd5d1bbe4c612e18893dacbfba96591d8b4
angel gutierrez <56939943+angelpyy@users.noreply.github.com>
2023-06-02 19:58:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,25 @@ from tkinter import ttk
 
 class Window:
     def __init__(self):
-        # init the window and small things
+        # Initialize the window and small things
         self.root = tk.Tk()
         self.root.geometry("1200x800")
         self.root.title("Stop Spending on Cardboard")
 
-        self.canvas = tk.Canvas(self.root, width=300, height=200, background='white')
+        # Create a scrollbar
+        scrollbar = tk.Scrollbar(self.root)
+        scrollbar.pack(side='right', fill='y')
+
+        # Create a canvas with scrollbar
+        self.canvas = tk.Canvas(self.root, width=300, height=200, background='white', yscrollcommand=scrollbar.set)
+        self.canvas.pack(side='left', fill='both', expand=True)
+
+        # Configure the scrollbar to scroll the canvas
+        scrollbar.config(command=self.canvas.yview)
+
+        # Create a frame inside the canvas for the labels
+        frame = tk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=frame, anchor='nw')
 
         df = pd.read_csv('order history/mtg_stocks.csv')
         # Iterate over the rows of the DataFrame
@@ -21,14 +34,10 @@ class Window:
             # Iterate over the values in each row
             for j, val in enumerate(row):
                 # Create a label widget for each value
-                label = tk.Label(self.canvas, text=val)
+                label = tk.Label(frame, text=val)
                 label.grid(row=i, column=j)
 
-        self.canvas.grid(row=0, column=0)
-
-        self.canvas.pack(side='left')
         self.root.mainloop()
-
 
 # TODO: csv editor? for some reason? i'm not sure its really necessary for what i want
 Window()
